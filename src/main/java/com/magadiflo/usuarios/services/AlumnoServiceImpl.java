3eb5eb8e71e9a18863146a42f882707e23b6c0a5
Martín <magadiflo@gmail.com>
2022-11-10 17:52:40 -0500

    Añadiendo librería commons para un service genérico y reutilizar código

--- src/main/java/com/magadiflo/usuarios/services/AlumnoServiceImpl.java
+++ src/main/java/com/magadiflo/usuarios/services/AlumnoServiceImpl.java
@@ -1,44 +1,16 @@
 package com.magadiflo.usuarios.services;
 
-import java.util.Optional;
-
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import com.magadiflo.commons.services.CommonServiceImpl;
 import com.magadiflo.usuarios.models.entity.Alumno;
 import com.magadiflo.usuarios.models.repository.IAlumnoRepository;
 
 @Service
-public class AlumnoServiceImpl implements IAlumnoService {
+public class AlumnoServiceImpl extends CommonServiceImpl<Alumno, IAlumnoRepository> implements IAlumnoService {
 
-	private final IAlumnoRepository alumnoRepository;
-
-	public AlumnoServiceImpl(IAlumnoRepository alumnoRepository) {
-		this.alumnoRepository = alumnoRepository;
-	}
-
-	@Override
-	@Transactional(readOnly = true)
-	public Iterable<Alumno> findAll() {
-		return this.alumnoRepository.findAll();
-	}
-
-	@Override
-	@Transactional(readOnly = true)
-	public Optional<Alumno> findById(Long id) {
-		return this.alumnoRepository.findById(id);
-	}
-
-	@Override
-	@Transactional
-	public Alumno save(Alumno alumno) {
-		return this.alumnoRepository.save(alumno);
-	}
-
-	@Override
-	@Transactional
-	public void deleteById(Long id) {
-		this.alumnoRepository.deleteById(id);
+	public AlumnoServiceImpl(IAlumnoRepository repository) {
+		super(repository);
 	}
 
 }
--- src/main/java/com/magadiflo/usuarios/services/IAlumnoService.java
+++ src/main/java/com/magadiflo/usuarios/services/IAlumnoService.java
@@ -1,15 +1,8 @@
 package com.magadiflo.usuarios.services;
 
-import java.util.Optional;
-
+import com.magadiflo.commons.services.ICommonService;
 import com.magadiflo.usuarios.models.entity.Alumno;
 
-public interface IAlumnoService {
-	Iterable<Alumno> findAll();
+public interface IAlumnoService extends ICommonService<Alumno> {
 
-	Optional<Alumno> findById(Long id);
-
-	Alumno save(Alumno alumno);
-
-	void deleteById(Long id);
 }
