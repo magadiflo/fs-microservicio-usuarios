7da93103a7c300887404ef036e4cf6368322e9b1
Martín <magadiflo@gmail.com>
2022-11-15 00:18:04 -0500

    Añadiendo métodos handlers con soporte multipartfile para post y put

--- src/main/java/com/magadiflo/usuarios/controllers/AlumnoController.java
+++ src/main/java/com/magadiflo/usuarios/controllers/AlumnoController.java
@@ -1,5 +1,6 @@
 package com.magadiflo.usuarios.controllers;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import javax.validation.Valid;
@@ -9,9 +10,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.magadiflo.commons.alumnos.models.entity.Alumno;
 import com.magadiflo.commons.controllers.CommonController;
@@ -49,5 +53,65 @@ public class AlumnoController extends CommonController<Alumno, IAlumnoService> {
 	public ResponseEntity<?> filtrar(@PathVariable String termino) {
 		return ResponseEntity.ok(this.service.encontrarPorNombreOrApellido(termino));
 	}
+	
+	/**
+	 * En el CommonsController, el método crear tiene anotado un 
+	 * @throws IOException 
+	 * @RequestBody porque nos envían un JSON con los datos del Entity,
+	 * en este caso del Alumno y ese sería el tipo de request, el
+	 * tipo de contenido: un JSON.
+	 * 
+	 * Peron en este método crearConFoto(...) nos envían parámetros, y 
+	 * entre los parámetros está la foto, por lo tanto el @RequestBody no va,
+	 * no se incluye en el método, es decir sería así: 
+	 * ....crearConFoto(@Valid Alumno entity....)
+	 * Lo que sí debemos agregar es un MultipartFile con la imagen.
+	 * 
+	 * Este método crearConFoto, es para recibir la foto y asignarla al
+	 * alumno y en seguida pasarlo a la clase padre para que haga el 
+	 * respectivo guardado, es decir estamos reutilizando el código 
+	 * de guardar
+	 */
+
+	@PostMapping(path = "/crear-con-foto")
+	public ResponseEntity<?> crearConFoto(@Valid Alumno alumno, BindingResult result, 
+			@RequestParam MultipartFile archivo) throws IOException {
+		if(!archivo.isEmpty()) {
+			alumno.setFoto(archivo.getBytes());
+		}
+		return super.crear(alumno, result);
+	}
+	
+	// Aquí tampoco va el @RequestBody, cuando estamos recibiendo un archivo 
+	// (imagen, foto, etc.) el tipo de request no es un JSON, 
+	// es otro tipo de request, es un Multipart Form Data.
+	// De forma automática el Alumno se poblará con los campos: nombre, apellido, email
+	// y la imagen se poblará en el MultipartFile archivo
+	@PutMapping(path = "/editar-con-foto/{id}")
+	public ResponseEntity<?> editarConFoto(@PathVariable Long id, 
+			@Valid Alumno alumno, BindingResult result, @RequestParam MultipartFile archivo) throws IOException {
+
+		if (result.hasErrors()) {
+			return this.validar(result);
+		}
+
+		Optional<Alumno> opAlumno = this.service.findById(id);
+		if (opAlumno.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+
+		Alumno alumnoBD = opAlumno.get();
+		alumnoBD.setNombre(alumno.getNombre());
+		alumnoBD.setApellido(alumno.getApellido());
+		alumnoBD.setEmail(alumno.getEmail());
+		
+		if(!archivo.isEmpty()) {
+			alumnoBD.setFoto(archivo.getBytes());
+		}
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(alumnoBD));
+	}
+	
+	
 
 }
