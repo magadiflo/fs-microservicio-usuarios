9b5f0fd4337b8a487983dac2c51171b6010a150f
Martín <magadiflo@gmail.com>
2022-11-14 22:59:21 -0500

    Añadiendo paginación

--- src/main/java/com/magadiflo/usuarios/models/repository/IAlumnoRepository.java
+++ src/main/java/com/magadiflo/usuarios/models/repository/IAlumnoRepository.java
@@ -3,11 +3,11 @@ package com.magadiflo.usuarios.models.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.magadiflo.commons.alumnos.models.entity.Alumno;
 
-public interface IAlumnoRepository extends CrudRepository<Alumno, Long> {
+public interface IAlumnoRepository extends PagingAndSortingRepository<Alumno, Long> {
 
 	@Query("SELECT a FROM Alumno AS a WHERE a.nombre LIKE %?1% OR a.apellido LIKE %?1%")
 	List<Alumno> encontrarPorNombreOrApellido(String termino);
